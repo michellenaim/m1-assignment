777b13abe6d2b96eea7e59d8f644e7b26eb1c4bc
Michelle Naim <michellenaim@gmail.com>
2021-07-30 19:34:13 -0700

    Add name of dog to lightbox

--- js/lightbox.js
+++ js/lightbox.js
@@ -17,40 +17,52 @@ $(document).ready(function () {
 
     if (altvalue == "Lexy") {
       var img = $("#album:nth-child(3) li:nth-child(1) img").clone();
-      $(".box").append(`<p id='photo-title'>${altvalue}</p>`)
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Max") {
       var img = $("#album:nth-child(3) li:nth-child(2) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Berta") {
       var img = $("#album:nth-child(3) li:nth-child(3) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Dorothy") {
       var img = $("#album:nth-child(3) li:nth-child(4) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Belle") {
       var img = $("#album:nth-child(3) li:nth-child(5) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Ivy") {
       var img = $("#album:nth-child(3) li:nth-child(6) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Coco") {
       var img = $("#album:nth-child(3) li:nth-child(7) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Ella") {
       var img = $("#album:nth-child(3) li:nth-child(8) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Felix") {
       var img = $("#album:nth-child(3) li:nth-child(9) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Jade") {
       var img = $("#album:nth-child(3) li:nth-child(10) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Kiwi") {
       var img = $("#album:nth-child(3) li:nth-child(11) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     } else if (altvalue == "Milo") {
       var img = $("#album:nth-child(3) li:nth-child(12) img").clone();
+      $(".box").append(`<p id='photo-title'>${altvalue}</p>`);
       $(".box").append(img);
     }
   });
